9f27dd13f38c0c3a75baf561a3c79c110622d802
liamkinne <liam@kinne.io>
2022-02-06 23:28:34 +1000

    add unit test

---
+++ golang/canbus/types_test.go
@@ -0,0 +1,13 @@
+package canbus
+
+import (
+	"testing"
+)
+
+func TestMessageInvalid(t *testing.T) {
+	msg := Message{}
+
+	if err := msg.Validate(); err == nil {
+		t.Errorf("Message should not be valid: %s", err)
+	}
+}
