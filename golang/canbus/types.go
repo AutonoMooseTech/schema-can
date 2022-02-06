1440cef6e077bc6acc810336a23fe0a057b262c0
liamkinne <liam@kinne.io>
2022-02-06 23:28:23 +1000

    fix some errors in the message type and add validation function

--- golang/canbus/types.go
+++ golang/canbus/types.go
@@ -1,5 +1,9 @@
 package canbus
 
+import (
+	"errors"
+)
+
 // Common object attributes
 type Object struct {
 	APIVersion string `yaml:"apiVersion"`
@@ -16,14 +20,15 @@ type Message struct {
 	Object `yaml:",inline"`
 	Spec   struct {
 		Id struct {
-			Standard int16    `yaml:"standard,omitempty"`
-			Extended int16    `yaml:"extended,omitempty"`
-			J1939 struct {
-				Priority      uint8 `yaml:"priority,omitempty"`
-				DataPage      uint8 `yaml:"data_page,omitempty"`
-				PDUFormat     uint8 `yaml:"pdu_format,omitempty"`
-				PDUSpecific   uint8 `yaml:"pdu_specific,omitempty"`
-				SourceAddress uint8 `yaml:"source_address,omitempty"`
+			Standard *uint16 `yaml:"standard,omitempty"`
+			Extended *uint32 `yaml:"extended,omitempty"`
+			J1939    *struct {
+				Priority         *uint8 `yaml:"priority,omitempty"`
+				DataPage         *bool  `yaml:"data_page,omitempty"`
+				ExtendedDataPage *bool  `yaml:"extended_data_page,omitempty"`
+				PDUFormat        *uint8 `yaml:"pdu_format,omitempty"`
+				PDUSpecific      *uint8 `yaml:"pdu_specific,omitempty"`
+				SourceAddress    *uint8 `yaml:"source_address,omitempty"`
 			} `yaml:"j1939,omitempty"`
 		} `yaml:"id"`
 		Length uint8 `yaml:"length"`
@@ -34,6 +39,32 @@ type Message struct {
 			Padding       uint8  `yaml:"padding,omitempty"`
 		} `yaml:"data" json:"data"`
 	} `yaml:"spec"`
+}
+
+func (msg *Message) Validate() (err error) {
+	// Check identifier is valid
+	if msg.Spec.Id.Standard != nil {
+		// id within bounds
+		if *msg.Spec.Id.Standard > ((2 ^ 11) - 1) {
+			err = errors.New("Standard identifier value is too large")
+		}
+	} else if msg.Spec.Id.Extended != nil {
+		// id within bounds
+		if *msg.Spec.Id.Extended > ((2 ^ 29) - 1) {
+			err = errors.New("Extended identifier value is too large")
+		}
+	} else if msg.Spec.Id.J1939 != nil {
+		// priority within bounds
+		if *msg.Spec.Id.J1939.Priority > 3 {
+			err = errors.New("Priority is too high. Maximum is 3")
+		}
+
+		// other fields are constrained by their types
+	} else {
+		err = errors.New("At least one identifier type must be declared")
+	}
+
+	return err
 }
 
 // Scaling, Limit, Transfer, Offset
