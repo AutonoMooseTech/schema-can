9d04f09784b6d29cda8040cd0fa2a0d961687c7b
liamkinne <liam@kinne.io>
2022-03-05 00:13:52 +1000

    add empty slot unit test

--- golang/types_test.go
+++ golang/types_test.go
@@ -8,6 +8,15 @@ func TestMessageInvalid(t *testing.T) {
 	msg := Message{}
 
 	if err := msg.Validate(); err == nil {
-		t.Errorf("Message should not be valid: %s", err)
+		t.Errorf("Defualt initialised Message should not be valid: %s", err)
 	}
 }
+
+func TestSlotInvalid(t *testing.T) {
+	slot := SLOT{}
+
+	if err := slot.Validate(); err == nil {
+		t.Errorf("Defualt initialised SLOT should not be valid: %s", err)
+	}
+}
+
